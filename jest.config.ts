d4a5d49c18b56ffa084170ab3bcaa19506fff861
sri charan <sricharan@users.noreply.github.com>
2026-05-31 09:51:59 +0530

    add coverage reporting

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
+  testMatch: ["**/*.test.ts"]
+};
